1b4bb6834e98464dce094fa0a441611376ae0d10
lee <isme@live.com>
2023-09-09 19:29:19 +0800

    恢复对比字段结构

--- restore.go
+++ restore.go
@@ -51,6 +51,10 @@ func main() {
     if _, ok := tables[tmp[1]];ok==false {
       create_table(tmp[1], strings.Replace(file, "backup", "scheme", 1))
       tables[tmp[1]] = tmp[1]
+
+      //如果存在，对比结构是否有差异
+    } else {
+      alter_table(tmp[1], strings.Replace(file, "backup", "scheme", 1))
     }
 
     log.Println(file)
@@ -81,44 +85,16 @@ var field_type = map[string]string{
   "year":"time",
 }
 func create_table(table string, scheme string) {
-  rows := table_scheme(scheme)
+  rows := load_scheme(scheme)
 
 
   _key := ""
   _sql := "CREATE TABLE "+table+"("
   for _, field := range rows {
 
-    _type := field["type"]
-    _idx  := strings.Index(_type, "(")
-    if _idx>-1 {
-      _type = _type[0:_idx]
-    }
-
-    _null := "NULL"
-    if field["null"]=="NO" {
-      _null = " NOT NULL"
-    }
-
-    _default := ""
-    if field["default"]!="" {
-      if _t, ok := field_type[_type];ok {
-        if _t=="text" {
-            _default = " DEFAULT '"+field["default"]+"'"
-        } else if(_t=="time") {
-            re, err := regexp.Compile("[0-9]")
-            if err != nil {
-              log.Fatal(err)
-            }
-            if re.MatchString(field["default"]) {
-                _default = " DEFAULT '"+field["default"]+"'"
-            } else {
-                _default = " DEFAULT "+field["default"]
-            }
-        } else {
-            _default = " DEFAULT "+field["default"]
-        }
-      }
-    }
+    _type    := _field_type(field["type"])
+    _null    := _field_null(field["null"])
+    _default := _field_default(_type, field["default"])
 
     if field["key"]=="PRI" {
       _key = field["field"]
@@ -137,7 +113,109 @@ func create_table(table string, scheme string) {
   log.Println(_sql)
 
 
-  //sql_execute(_sql)
+  sql_execute(_sql)
+}
+
+func _field_null(null string) string {
+  if null=="NO" {
+      return " NOT NULL"
+  }
+
+  return " NULL"
+}
+
+func _field_type(field_type string) string {
+    _idx  := strings.Index(field_type, "(")
+    if _idx>-1 {
+      field_type = field_type[0:_idx]
+    }
+
+    return field_type
+}
+
+func _field_default(_type string, default_value string) string {
+    _default := ""
+    if default_value!="" {
+      if _t, ok := field_type[_type];ok {
+        if _t=="text" {
+            _default = " DEFAULT '"+default_value+"'"
+        } else if(_t=="time") {
+            re, err := regexp.Compile("[0-9]")
+            if err != nil {
+              log.Fatal(err)
+            }
+            if re.MatchString(default_value) {
+                _default = " DEFAULT '"+default_value+"'"
+            } else {
+                _default = " DEFAULT "+default_value
+            }
+        } else {
+            _default = " DEFAULT "+default_value
+        }
+      }
+    }
+
+    return _default
+}
+
+func alter_table(table string, scheme string) {
+  new_scheme := load_scheme(scheme)
+  old_scheme := table_scheme(table)
+
+  //log.Println(new_scheme)
+  //log.Println(old_scheme)
+
+  for _, new_field := range new_scheme {
+    //新增字段
+    if _, ok := old_scheme[new_field["field"]];ok==false {
+          _type    := _field_type(new_field["type"])
+          _null    := _field_null(new_field["null"])
+          _default := _field_default(_type, new_field["default"])
+
+          _sql := "ALTER TABLE "+table+" ADD `"+new_field["field"]+"` "+new_field["type"]+_null+" "+new_field["extra"]+" "+_default+";"
+          log.Println(_sql)
+      //新增字段
+      continue
+    }
+
+    //旧字段对比差异
+    old_field := old_scheme[new_field["field"]]
+    if new_field["type"]!=old_field["type"] || new_field["null"]!=old_field["null"] || new_field["default"]!=old_field["default"] {
+          _type    := _field_type(new_field["type"])
+          _null    := _field_null(new_field["null"])
+          _default := _field_default(_type, new_field["default"])
+
+          _sql := "ALTER TABLE "+table+" CHANGE `"+old_field["field"]+"` `"+new_field["field"]+"` "+new_field["type"]+_null+" "+new_field["extra"]+" "+_default+";"
+          log.Println(_sql, old_field, new_field)
+    }
+  }
+}
+
+func table_scheme(table string) map[string]map[string]string {
+    rows, err := db.Query("describe "+table)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    scheme := map[string]map[string]string{}
+    var rowField, rowType, rowNull, rowKey, rowExtra string
+    var rowDefault sql.RawBytes
+    for rows.Next() {
+      if err := rows.Scan(&rowField, &rowType, &rowNull, &rowKey, &rowDefault, &rowExtra); err != nil {
+            panic(err.Error())
+        }
+
+      scheme[rowField] = map[string]string{
+        "field"  : rowField,
+        "type"   : rowType,
+        "null"   : rowNull,
+        "key"    : rowKey,
+        "default": string(rowDefault),
+        "extra"  : rowExtra,
+      }
+    }
+
+    return scheme
 }
 
 func sql_execute(_sql string) {
@@ -155,7 +233,7 @@ func sql_execute(_sql string) {
   log.Println(res)
 }
 
-func table_scheme(scheme string) []map[string]string {
+func load_scheme(scheme string) []map[string]string {
   _, err := os.Stat(scheme)
   if err != nil && os.IsNotExist(err) {
       log.Fatal("表结构文件不存在：", scheme)
